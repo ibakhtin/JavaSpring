082c168737a64fa35d0f3dc0abf486a91a12b04d
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-01 14:58:22 +0300

    Added Area, CharDemo, CharDemo2 classes.

---
+++ JavaByHerbertSchildt/Chapter03/src/Area.java
@@ -0,0 +1,18 @@
+/**
+ * Created by ibakhtin on 01.03.17.
+ */
+
+// Compute the area of a circle.
+
+public class Area {
+    public static void main(String[] args) {
+        double pi, radius, area;
+
+        radius = 10.8; // radius of circle
+        pi = 3.14159265; // pi, approximately
+
+        area = pi * radius * radius;
+
+        System.out.println("The area of circle is " + area);
+    }
+}
