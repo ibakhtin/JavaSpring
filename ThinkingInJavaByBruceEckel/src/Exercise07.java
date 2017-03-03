777d84a0ee70a12ab7e4597e9ea85c7e5c7e0232
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 15:58:57 +0300

    Added Exercise07 class.

---
+++ ThinkingInJavaByBruceEckel/src/Exercise07.java
@@ -0,0 +1,13 @@
+/**
+ * Created by ibakhtin on 03.03.17.
+ */
+
+// Exercise 7
+
+class SampleClass07 {}
+
+public class Exercise07 {
+    public static void main(String[] args) {
+        SampleClass07 obj = new SampleClass07();
+    }
+}
