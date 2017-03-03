2b253c3fba4dd76a13563144d6935de395776696
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 20:35:35 +0300

    Added ForTick2 class.

---
+++ JavaByHerbertSchildt/Chapter05/src/ForTick2.java
@@ -0,0 +1,14 @@
+/**
+ * Created by ibakhtin on 03.03.17.
+ */
+
+// Declare a loop control variable inside the for.
+
+public class ForTick {
+    public static void main(String[] args) {
+        // here, n is declared inside of the for loop
+        for (n = 10; n > 0; n--) {
+            System.out.println("tick " + n);
+        }
+    }
+}
