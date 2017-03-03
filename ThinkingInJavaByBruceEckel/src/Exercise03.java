c8e2db92b28f21f00222605b02ddc40086ddbedf
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 15:18:01 +0300

    Added Exercise03 class.

---
+++ ThinkingInJavaByBruceEckel/src/Exercise03.java
@@ -0,0 +1,19 @@
+/**
+ * Created by ibakhtin on 03.03.17.
+ */
+
+// Exercise 3
+
+class SampleClass03 {
+    SampleClass03() {
+        System.out.println("Hello from default constructor.");
+    }
+}
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        // SampleClass03 obj = new SampleClass03();
+        // or
+        new SampleClass03();
+    }
+}
