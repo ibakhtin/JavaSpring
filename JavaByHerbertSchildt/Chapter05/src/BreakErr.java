98042f03c17e680cf586fec3bc5be83aa61ceef0
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 22:35:22 +0300

    Added BreakErr class.

---
+++ JavaByHerbertSchildt/Chapter05/src/BreakErr.java
@@ -0,0 +1,19 @@
+/**
+ * Created by ibakhtin on 03.03.17.
+ */
+
+// This program contains an error.
+
+public class BreakErr {
+    public static void main(String[] args) {
+
+        one: for (int i = 0; i < 3; i++) {
+            System.out.println("Pass " + i + ": ");
+        }
+
+        for (int j = 0; j < 100; j++) {
+            if(j == 10) break one;
+            System.out.println(j + " ");
+        }
+    }
+}
