7246e17d4037c6600f760ad7e6872fb5ba468db9
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 13:40:11 +0300

    Added DefaultConstructor class.

---
+++ ThinkingInJavaByBruceEckel/src/DefaultConstructor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by ibakhtin on 03.03.17.
+ */
+
+// Demonstrate default constructor.
+
+class Bird {}
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Bird bird = new Bird(); // Default!
+    }
+}
