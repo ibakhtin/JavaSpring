0ba9976d407be46f68de0c72537bcc629da9d378
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 20:36:29 +0300

    Fix ForTick2 class.

--- JavaByHerbertSchildt/Chapter05/src/ForTick2.java
+++ JavaByHerbertSchildt/Chapter05/src/ForTick2.java
@@ -4,10 +4,10 @@
 
 // Declare a loop control variable inside the for.
 
-public class ForTick {
+public class ForTick2 {
     public static void main(String[] args) {
         // here, n is declared inside of the for loop
-        for (n = 10; n > 0; n--) {
+        for (int n = 10; n > 0; n--) {
             System.out.println("tick " + n);
         }
     }
