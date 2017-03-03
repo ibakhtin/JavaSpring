6473fcb508d14a318ec00417eb49b4c089f555f0
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 21:22:38 +0300

    Added Comma, ForEach, ForEach2, ForVar, Sample classes.

---
+++ JavaByHerbertSchildt/Chapter05/src/Comma.java
@@ -0,0 +1,16 @@
+/**
+ * Created by ibakhtin on 03.03.17.
+ */
+
+// Using the comma.
+
+public class Comma {
+    public static void main(String[] args) {
+        int a, b;
+
+        for (a = 1, b = 4; a < b; a++, b--) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
