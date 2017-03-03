7a6f3947404b11aedbc10040e9d72eb7eb2b02ea
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-03 21:47:51 +0300

    Added Nested, Search classes.

---
+++ JavaByHerbertSchildt/Chapter05/src/Nested.java
@@ -0,0 +1,18 @@
+/**
+ * Created by ibakhtin on 03.03.17.
+ */
+
+// Loops may be nested.
+
+public class Nested {
+    public static void main(String[] args) {
+        int i, j;
+
+        for (i = 0; i < 10; i++) {
+            for (j = i; j < 10; j++) {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
+}
