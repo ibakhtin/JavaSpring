44dfd1369231145c224759729c45bc26713247ef
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-04 19:26:50 +0300

    Added FirtsSample class.

---
+++ src/CoreJavaByCaySHorstmann/Chapter03/FirstSample.java
@@ -0,0 +1,9 @@
+package CoreJavaByCaySHorstmann.Chapter03;
+
+// FirstSample class.
+
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
