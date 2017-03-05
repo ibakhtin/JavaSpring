0ab73cc741df414c164537906476995594d99d49
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-06 00:12:25 +0300

    Added BoxDemo class.

---
+++ src/JavaByHerbertSchildt/Chapter06/BoxDemo.java
@@ -0,0 +1,28 @@
+package JavaByHerbertSchildt.Chapter06;
+
+/**
+ * Created by ibakhtin on 06.03.17.
+ */
+
+// A program that uses the Box class.
+
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        double vol;
+
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        vol = myBox.width * myBox.height * myBox.depth;
+
+        System.out.println("Volume is " + vol);
+    }
+}
