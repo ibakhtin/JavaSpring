4ca268fbba5f07e49bcdf40d2bebc11fe09b9162
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-06 00:43:35 +0300

    Added BoxDemo3, Box3 classes.

---
+++ src/JavaByHerbertSchildt/Chapter06/Box3.java
@@ -0,0 +1,18 @@
+package JavaByHerbertSchildt.Chapter06;
+
+/**
+ * Created by ibakhtin on 06.03.17.
+ */
+
+// Box3 class.
+
+class Box3 {
+    double width;
+    double height;
+    double depth;
+
+    void volume() {
+        System.out.print("Volume is ");
+        System.out.println(width * height * depth);
+    }
+}
