3d6e6fa7a4c83a69480ce5a19cc0f28b25b78281
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-06 12:47:16 +0300

    Added some classes.

---
+++ src/JavaByHerbertSchildt/Chapter06/TestStack.java
@@ -0,0 +1,27 @@
+package JavaByHerbertSchildt.Chapter06;
+
+/**
+ * Created by ibakhtin on 06.03.17.
+ */
+
+// TestStack class.
+
+public class TestStack {
+    public static void main(String[] args) {
+        Stack stackA = new Stack();
+        Stack stackB = new Stack();
+
+        // push some numbers onto the stack
+        for (int i = 0; i < 10; i++) stackA.push(i);
+        for (int i = 10; i < 20; i++) stackB.push(i);
+
+        // pop those numbers off the stack
+        System.out.println("Stack in stackA:");
+        for (int i = 0; i < 10; i++)
+            System.out.println(stackA.pop());
+
+        System.out.println("Stack in stackB:");
+        for (int i = 0; i < 10; i++)
+            System.out.println(stackB.pop());
+    }
+}
