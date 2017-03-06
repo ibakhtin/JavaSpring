739a52fb42d61f2d1a1d1b74fbe173de7f78a392
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-06 23:39:35 +0300

    Added some classes.

---
+++ src/JavaByHerbertSchildt/Chapter07/VarArgs2.java
@@ -0,0 +1,24 @@
+package JavaByHerbertSchildt.Chapter07;
+
+/**
+ * Created by ibakhtin on 06.03.17.
+ */
+
+// Use varargs with standard arguments.
+
+public class VarArgs2 {
+    static void vaTest(String msg, int ... v) {
+        System.out.print(msg + v.length + " Contents: ");
+
+        for (int x : v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        vaTest("One vararg: ", 10);
+        vaTest("Three varargs: ", 1, 2, 3);
+        vaTest("No varargs: ");
+    }
+}
