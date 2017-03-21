5158d9833076342514c022c2fa06f74af4b4a15c
Igor Bakhtin <ibakhtin@icloud.com>
2017-03-21 16:54:14 +0300

    Added some files.

--- src/javabyherbertschildt/chapter16/stringbuffer/IndexDemo.java
+++ src/javabyherbertschildt/chapter16/stringbuffer/IndexDemo.java
@@ -3,6 +3,7 @@ package javabyherbertschildt.chapter16.stringbuffer;
 /**
  * Created by ibakhtin on 20.03.17.
  */
+
 public class IndexDemo {
     public static void main(String[] args) {
         StringBuffer stringBuffer = new StringBuffer("one two one");
